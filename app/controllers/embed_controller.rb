5a482814752e94b1fd69da42abc5bde39746f7ef
Luigi Montanez <luigi.montanez@gmail.com>
2010-02-17 14:33:49 -0500

    Introduce the notion of widget sizes. [#2522043]

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -2,6 +2,22 @@ class EmbedController < ApplicationController
 
   def embed
     @params = params
+    @size = params[:s] || 'med'
+    case @size
+      when 'sm'
+        @width = 160
+        @height = 305
+      when 'med'
+        @width = 300
+        @height = 250
+      when 'lg'
+        @width = 400
+        @height = 300
+      else
+        @width = 300
+        @height = 250
+    end
+         
     render :template => 'embed/embed.js.erb', :content_type => 'text/javascript'
   end
 
