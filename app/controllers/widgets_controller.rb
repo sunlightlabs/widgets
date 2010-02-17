ebd62f12c9c3ba7a1f2253171fe40572ed7b9459
Luigi Montanez <luigi.montanez@gmail.com>
2010-02-17 14:34:09 -0500

    Implement Ali's bio widget. [#2522043]

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -2,6 +2,8 @@ class WidgetsController < ApplicationController
 
   def show
     @slug = params[:id]
+    @legislator = Sunlight::Legislator.all_where(:bioguide_id => params[:bioguide_id]).first
+
     case @slug 
     when 'bio'
       @full_name = "Simple Bio"
@@ -9,6 +11,7 @@ class WidgetsController < ApplicationController
   end
 
   def embed
+    @legislator = Sunlight::Legislator.all_where(:bioguide_id => params[:bioguide_id]).first
     render :layout => false
   end
 
