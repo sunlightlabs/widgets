3d2bacea7d528de55884c226272a1788cef57422
Luigi Montanez <luigi.montanez@gmail.com>
2010-02-23 12:51:00 -0500

    Bio widget: Implement state background. Use "Senator" instead of "Sen.".

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def long_title title
+    return "Representative" if title == "Rep"
+    return "Senator" if title == "Senator"
+    return "Delegate" if title == "Del"
+  end
+
 end
