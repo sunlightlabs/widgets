f0e3e147aab81849297f3e9d05a45f599bded31a
Luigi Montanez <luigi.montanez@gmail.com>
2010-03-01 17:24:39 -0500

    No need to use @slug. Give bill widget a friendly name [#2640165]

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,12 +1,13 @@
 class WidgetsController < ApplicationController
 
   def show
-    @slug = params[:id]
     @legislator = Sunlight::Legislator.all_where(:bioguide_id => params[:bioguide_id]).first
 
-    case @slug 
+    case params[:id] 
     when 'bio'
       @full_name = "Simple Bio"
+    when 'bill'
+      @full_name = "Bill Voting Report"
     end 
   end
 
