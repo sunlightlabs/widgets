ef545b09e93524b4b54108c8efe2d67b7e91a86b
klondike <kprojection@gmail.com>
2010-03-08 14:10:49 -0500

    Implemented the search page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,34 @@ module ApplicationHelper
     }[title.downcase.to_sym]
   end
 
+  def photo_url_for(legislator, size = :small)
+    sizes = {:small => "40x50", :medium => "100x125", :large => "200x250"}
+    "http://assets.sunlightfoundation.com/moc/#{sizes[size]}/#{legislator.bioguide_id}.jpg"
+  end
+  
+  def title_for(legislator)
+    {"Rep" => "Representative",
+     "Sen" => "Senator",
+     "Del" => "Delegate"}[legislator.title] || "Delegate"
+  end
+  
+  def party_for(legislator)
+    {"D" => "Democratic",
+     "R" => "Republican",
+     "I" => "Independent"}[legislator.party]
+  end
+  
+  def district_for(legislator)
+    "#{legislator.state}-#{district_for legislator}"
+  end
+  
+  def district_for(legislator)
+    legislator.title == 'Sen' ? legislator.district.gsub(' Seat', '') : legislator.district
+  end
+  
+  def search_name_for(legislator)
+    name = legislator.lastname
+    name << " #{legislator.name_suffix}" if legislator.name_suffix.present?
+    name << ", #{legislator.nickname.present? ? legislator.nickname : legislator.firstname}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Widgets::Application.routes.draw do |map|
-
   root :to => 'main#index'
-  match 'search' => 'main#search', :as => 'search'
-  match 'contact' => 'main#contact', :as => 'contact'
+  
   match 'about' => 'main#about', :as => 'about'
+  match 'contact' => 'main#contact', :as => 'contact'
+  
+  match 'search' => 'legislators#index', :as => 'search'
 
   match 'embed' => 'embed#embed', :as => 'embed'
-
   resources :widgets do
     member do
       get :embed
