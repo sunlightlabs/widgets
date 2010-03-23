46fb5f6140d87a538c5d9854da3953682ef3252d
klondike <kprojection@gmail.com>
2010-03-23 16:20:12 -0400

    Lots of refactoring to widget embedding and the bill widget especially

--- public/javascripts/customize.js
+++ public/javascripts/customize.js
@@ -3,7 +3,11 @@ var sizes = {sm: [160, 300], med: [300, 250], lg: [400,300]};
 var widget_size = 'lg';
 var widget_color = '1F83B5';
 
-$(document).ready(function() {
+// should be filled in with any default values before the document is ready
+var custom = {}
+
+$(function() {
+  updateFrame();
 
   // widget size switcher
   $("li.switcherbtn a").click(function() {
@@ -27,13 +31,8 @@ $(document).ready(function() {
   $.farbtastic("#primaryColor", "#primaryColorText").setColor("#" + widget_color);
   
   // embed code field
-  $("button#generate").click(updateEmbedCode);
   $("#grabCode").click(function() {$(this).select()});
   
-  
-  // pre-populate
-  updateFrame();
-  updateEmbedCode();
 });
 
 /* Depends on frontend_hostname, id, and bioguide_id being set before this file is included. */
@@ -41,14 +40,27 @@ function updateFrame() {
   var iframe_url = "http://" + frontend_hostname + "/widgets/" + widget_id + "/embed?bioguide_id=" + bioguide_id;
   
   iframe_url += "&size=" + widget_size + "&color=" + widget_color;
+  iframe_url += "&" + queryString(custom);
   
   $("#widgetConstruct iframe").attr("src", iframe_url)
     .attr("width", sizes[widget_size][0])
     .attr("height", sizes[widget_size][1]);
+  
+  updateEmbedCode();
 }
 
 function updateEmbedCode() {
   var embed_url = "http://" + frontend_hostname + "/embed?w=" + widget_id + "&bgd=" + bioguide_id + "&s=" + widget_size + "&color=" + widget_color;
-    
+  embed_url += "&" + queryString(custom);
+  
   $('input#grabCode').attr('value', "<script type='text\/javascript' src='" + embed_url + "'><\/script>");
+}
+
+// not currently doing any http encoding
+function queryString(object) {
+  var query = "";
+  $.each(object, function(k, v) {
+    query += k + "=" + v;
+  });
+  return query;
 }
--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -2,6 +2,20 @@
  * Functions used across all widgets
  */
 
-function profileImage(bioguideId) {
-  return "http://assets.sunlightfoundation.com/moc/100x125/" + bioguideId + ".jpg";
+function profileImage(bioguide) {
+  return "http://assets.sunlightfoundation.com/moc/100x125/" + bioguide + ".jpg";
+}
+
+// returns a string suitable for feeding into JQuery's $.getJSON function (using a ? for the JSONP callback)
+function drumboneUrl(method, sections, options) {
+  return data_endpoint + "/" + method + ".json?apikey=" + sunlight_api_key + "&sections=" + sections.join(",") + "&" + queryString(options) + "&callback=?";
+}
+
+// not currently doing any http encoding
+function queryString(object) {
+  var query = "";
+  $.each(object, function(k, v) {
+    query += k + "=" + v;
+  });
+  return query;
 }
