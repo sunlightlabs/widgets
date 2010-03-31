feb8704fc787a04617eda1533636b26444513a90
klondike <kprojection@gmail.com>
2010-03-31 18:01:43 -0400

    Refactored loadWidget to take in the components of a drumbone URL

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -1,14 +1,60 @@
 /**
- * Functions used across all widgets
+ * The main widget loading function. Makes use of several global variables set (by necessity) in
+ * app/views/widgets/embed.html.  If a snapshot_id is set, it'll use that, otherwise, it'll use the
+ * given Drumbone parameters to pull from the live site.
  */
+ 
+function loadWidget(method, sections, options, callback) {
+  var augmented = function(data) {
+    // if you wanted to check the data for an "error" key, here is where you would do it
+    callback(data); 
+    $("body").css("display", "block");
+  };
+  
+  
+  var url;
+  var data = {};
+  if (snapshot_id)
+    url = snapshotUrl(snapshot_directory, snapshot_id);
+  else {
+    url = drumboneUrl(data_endpoint, method);
+    data.apikey = sunlight_api_key;
+    data.sections = sections.join(",");
+    $.extend(data, options); 
+  }
+  
+
+  return $.ajax({
+    url: url, 
+    data: data,
+    dataType: "jsonp",
+    jsonpCallback: "politiwidgetsCallback",
+    success: augmented
+  });
+}
+
+
+// helpers
 
 function profileImage(bioguide) {
   return "http://assets.sunlightfoundation.com/moc/100x125/" + bioguide + ".jpg";
 }
 
 // returns a string suitable for feeding into JQuery's $.getJSON function (using a ? for the JSONP callback)
-function drumboneUrl(method, sections, options) {
-  return data_endpoint + "/" + method + ".json?apikey=" + sunlight_api_key + "&sections=" + sections.join(",") + "&" + queryString(options) + "&callback=?";
+function drumboneUrl(endpoint, method) {
+  return endpoint + "/" + method + ".json";
+}
+
+function snapshotUrl(endpoint, id) {
+  return endpoint + id + ".json";
+}
+
+function decimal_format(num, places) {
+  if (Math.floor(num) == num) {
+    return num;
+  } else {
+    return Highcharts.numberFormat(num, places);
+  }
 }
 
 // not currently doing any http encoding
@@ -18,19 +64,4 @@ function queryString(object) {
     query += k + "=" + v;
   });
   return query;
-}
-
-function loadWidget(url, callback) {
-  var augmented = function(data) {
-    // if you wanted to check the data for an "error" key, here is where you would do it
-    
-    callback(data); 
-    $("body").css("display", "block");
-  };
-  
-  return $.ajax({
-    url: url, 
-    dataType: "jsonp",
-    success: augmented
-  });
 }
