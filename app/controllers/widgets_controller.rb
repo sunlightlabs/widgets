ce225966822a9b2971f5a388008c3a04b9883c9c
klondike <kprojection@gmail.com>
2010-04-03 14:26:55 -0400

    Now saving snapshots to S3, depends on an S3 access key being set in settings.yml. Also figured out the right specification of slashes (sigh)

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,7 +3,38 @@ class WidgetsController < ApplicationController
   before_filter :load_legislator, :except => :snapshot
   
   def snapshot
-    render :text => "123456"
+    return unless params[:method] and params[:sections]
+    
+    drumbone = Drumbone.new settings[:sunlight_api_key]
+    drumbone.endpoint = settings[:data_endpoint]
+    drumbone.callback = "politiwidgetsCallback"
+    
+    json = drumbone.jsonp_for params[:method], params[:sections].split(","), params[:options]
+    
+    # timestamped, down to below the millisecond
+    id = (Time.now.to_f * 100000).to_i.to_s
+    
+    if settings[:s3][:access_key]
+      s3 = S3::Service.new(
+        :access_key_id => settings[:s3][:access_key], 
+        :secret_access_key => settings[:s3][:secret_key]
+      )
+      bucket = s3.buckets.find settings[:s3][:bucket]
+      
+      object = bucket.objects.build "#{settings[:s3][:path]}#{id}.json"
+      object.acl = :public_read
+      object.content_type = 'application/json'
+      object.content = json
+      
+      if object.save
+        render :text => id
+      else
+        head 500
+      end
+    else
+      head 500
+    end
+    
   end
   
   def embed
