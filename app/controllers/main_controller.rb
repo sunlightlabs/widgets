d0106b0550c4c7c21cbdea8a8153cc3c900af046
klondike <kprojection@gmail.com>
2010-04-09 11:04:34 -0400

    Switched an instance variable in the controller to a hook in the layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,15 +1,7 @@
 class MainController < ApplicationController
   
-  before_filter :hide_sidebar, :except => :index
-  
   def index
     @widget = widgets[settings[:featured][:widget]]
-  end
-  
-  protected
-  
-  def hide_sidebar
-    @hide_sidebar = true
   end
   
 end
