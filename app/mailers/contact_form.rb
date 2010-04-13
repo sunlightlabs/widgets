a4bce72377207d16a42bc92f32b94af07391be0b
klondike <kprojection@gmail.com>
2010-04-12 20:10:02 -0400

    Renamed subject line of contact submissions

--- app/mailers/contact_form.rb
+++ app/mailers/contact_form.rb
@@ -1,7 +1,7 @@
 class ContactForm < ActionMailer::Base
   
   def contact_form(name, email, message)
-    mail(:to => CONTACT_RECIPIENTS, :from => email, :subject => "Contact form submission from Politiwidgets") do |format|
+    mail(:to => CONTACT_RECIPIENTS, :from => email, :subject => "[Politiwidgets] Contact form submission from #{name}") do |format|
       format.text {render :text => message}
     end
   end
