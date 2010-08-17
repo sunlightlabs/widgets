1fe4e7e5d3c0b64cb5cef474bed2434d568014d6
klondike <kprojection@gmail.com>
2010-08-17 15:18:31 -0400

    Made a TransparencyData JavaScript library

---
+++ public/javascripts/transparency_data.js
@@ -0,0 +1,46 @@
+// JSONP library for TransparencyData.com, requires jQuery
+
+var TransparencyData = {
+  api_key: null,
+  base_url: "http://transparencydata.com/api/1.0",
+  jsonpCallback: null,
+
+  // takes a CRP ID, gets an entity id
+  // takes in a callback which will get passed a 
+  getEntityId: function(bioguide_id, callback) { 
+    return TransparencyData.getJSON("/entities/id_lookup.json", {
+      data: {bioguide_id: bioguide_id},
+      success: function(data) {
+        if (data && data.length > 0 && data[0].id)
+          callback(data[0].id);
+        else
+          callback(null);
+      }
+    });
+  },
+
+  // takes an entity id, gets top contributors
+  getContributors: function(entity_id, cycle, top, callback) {
+    return TransparencyData.getJSON("/aggregates/pol/" + entity_id + "/contributors.json", {
+      data: {cycle: cycle},
+      success: function(data) {
+        if (data && data.length > 0 && data[0].total_amount)
+          callback(data);
+        else
+          callback(null);
+      }
+    });
+  },
+  
+  getJSON: function(path, options) {
+    return $.ajax($.extend(true, options, {
+      url: TransparencyData.base_url + path,
+      data: {
+        apikey: TransparencyData.api_key
+      },
+      jsonpCallback: TransparencyData.jsonpCallback,
+      dataType: "jsonp"
+    }));
+  }
+  
+};
